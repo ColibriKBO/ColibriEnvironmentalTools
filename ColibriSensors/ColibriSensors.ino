f449b454a921e1832c87368d9faa2b853c6d00e4
mjmazur <mmazur5@uwo.ca>
2019-09-17 19:55:44 -0400

    Create ColibriSensors.ino

---
+++ ColibriSensors/ColibriSensors.ino
@@ -0,0 +1,19 @@
+// Code for the Colibri environmental sensors and relays in each dome.
+
+// Sensors List
+// ------------
+// Temperature
+// Humidity
+// Pressure
+// Rain
+// IR
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
